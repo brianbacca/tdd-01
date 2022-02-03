3d655267e7de8bb252bfc61430a2bff31cd96713
brianbacca <brianbn05@gmail.com>
2022-02-03 15:46:38 -0300

    refactor: add function fro each property VehicleModel

--- src/models/vehicle-model.ts
+++ src/models/vehicle-model.ts
@@ -1,10 +1,17 @@
 export class VehicleModel {
-  name: string;
-  model: string;
-  year: number;
-  constructor() {
-    this.name = "Nissan";
-    this.model = "DXT";
-    this.year = 2020;
+  constructor(
+    private readonly name: string,
+    private readonly model: string,
+    private readonly year: number
+  ) {}
+
+  getName(): string {
+    return this.name;
+  }
+  getModel(): string {
+    return this.model;
+  }
+  getYear(): number {
+    return this.year;
   }
 }
