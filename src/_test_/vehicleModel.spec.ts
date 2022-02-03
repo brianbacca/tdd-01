515c3aaf479ec354b090fa84fc569f2918e7b086
brianbacca <brianbn05@gmail.com>
2022-02-03 17:31:52 -0300

    refactor: add helper vehicleModel

--- src/_test_/vehicleModel.spec.ts
+++ src/_test_/vehicleModel.spec.ts
@@ -2,10 +2,18 @@ import { VehicleModel } from "../models/vehicle-model";
 
 describe("Classes", () => {
   it("Class VehicleModel", () => {
-    const vehicleModel = new VehicleModel('Nissan', 'DXT', 2020);
+    const vehicleModel = new VehicleModel({
+      name: "Nissan",
+      model: "DXT",
+      year: 2020,
+      price: 2000000,
+      Inventory: true,
+    });
 
     expect(vehicleModel.getName()).toEqual("Nissan");
     expect(vehicleModel.getModel()).toEqual("DXT");
     expect(vehicleModel.getYear()).toBe(2020);
+    expect(vehicleModel.getPrice()).toBe(2000000);
+    expect(vehicleModel.getInventory()).toEqual(true);
   });
 });
