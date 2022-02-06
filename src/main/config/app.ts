bccb9e9ecf020fec58131c3b9689a702118ffea6
brianbacca <brianbn05@gmail.com>
2022-02-06 17:33:14 -0300

    feat: config server and middlewares

---
+++ src/main/config/app.ts
@@ -0,0 +1,14 @@
+import express, { Router } from "express";
+import { config as dotenv } from "dotenv";
+import routes from "./routes";
+import middlewares from "./middlewares";
+const app = express();
+
+
+middlewares(app);
+
+//ROUTES INIT
+routes(app);
+dotenv();
+
+export default app;
---
+++ src/main/config/middlewares.ts
@@ -0,0 +1,7 @@
+import { Express } from "express";
+import { bodyParse, cors } from "../middlewares";
+
+export default (app: Express): void => {
+  app.use(bodyParse);
+  app.use(cors);
+};
